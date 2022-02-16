bddbeebdbfc221eb552523250405dc71b52f6f92
Aitor-examen <acarros2021@cepnet.net>
2022-02-16 09:55:20 +0100

    Estructures per emmagatzemar informació

---
+++ Ejercicio 2/main.js
@@ -0,0 +1,15 @@
+function mostrarObjeto() {
+    const persona = {
+        Nombre: "Aitor",
+        Apellidos: "Carro Sanz",
+        Edad: 23,
+        colorOjos: "Marrón/Verde"
+      };
+    
+      window.alert("Nombre: " + persona.Nombre +  
+      "\nApellido: " + persona.Apellidos + 
+      "\nEdad: " + persona.Edad +
+      "\nColor de ojos: " + persona.colorOjos 
+      );
+
+}
